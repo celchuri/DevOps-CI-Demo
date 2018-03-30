cbe7b813544fce46242fe009f404776a39226439
celchuri <celchuri@DIN80011979.corp.capgemini.com>
2018-03-30 12:47:44 +0530

    For loop added

--- Day2-Demo/src/main/java/SalesClass.java
+++ Day2-Demo/src/main/java/SalesClass.java
@@ -3,7 +3,11 @@ public class SalesClass {
 
 	public void show(){
 
-		int[] arg={12,45,67,90};	
+		int[] arg={12,45,67,90};
+		for(int i=0;i<arg.length;i++){
+			System.out.println(arg);
+		}
+		
 		System.out.println("Show Method");
 		
 	}
