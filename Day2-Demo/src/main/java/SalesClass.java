924891c096d31574bff955ef6cae0ab18265f4b7
celchuri <celchuri@DIN80011979.corp.capgemini.com>
2018-03-30 12:54:02 +0530

    Added the args

--- Day2-Demo/src/main/java/SalesClass.java
+++ Day2-Demo/src/main/java/SalesClass.java
@@ -5,7 +5,7 @@ public class SalesClass {
 
 		int[] arg={12,45,67,90};
 		for(int i=0;i<arg.length;i++){
-			System.out.println(arg);
+			System.out.println(arg[i]);
 		}
 		
 		System.out.println("Show Method");
